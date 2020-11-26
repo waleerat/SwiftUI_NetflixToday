afd94d76c8c1a95dbdb49a2a03b2db1355b6e808
Waleerat S. Gottlieb <waleerat.gottlieb@icloud.com>
2020-11-26 21:09:28 +0100

    24 Costom SearchBar part1

---
+++ iOS/Helpers/Colors.swift
@@ -0,0 +1,14 @@
+//
+//  Colors.swift
+//  NetflixToday
+//
+//  Created by Waleerat Gottlieb on 2020-11-26.
+//
+
+import Foundation
+import SwiftUI
+
+extension Color {
+    static let graySearhBackground = Color("gray_search_backgroud")
+    static let graySearchText = Color("gray_search_text")
+}
